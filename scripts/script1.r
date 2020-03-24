27dbb842a7d5933e6d183b68a1a551eac77c4faf
adam96op <adam_szkola@o2.pl>
2020-03-24 12:55:44 +0100

    AP 2020-03-24

--- scripts/script1.r
+++ scripts/script1.r
@@ -1,8 +1,10 @@
 #wlaczenie biblioteki
 library (tm)
+library (hunspell)
+library (stringr)
 
 #zmiana katologu roboczego
-workDir <- "D:\\AP\\TextMining"
+workDir <- "D:\\Adam_nowy\\TextMining\\TextMining"
 setwd(workDir)
 
 #definicja katalogu projektu
@@ -41,6 +43,48 @@ stoplist <-readLines(
 corpus <-tm_map(corpus, removeWords, stoplist)
 corpus <-tm_map(corpus, stripWhitespace)
 
+#usuniêcie em dash i 3/4
+
+remove_char <- content_transformer(function(x,pattern) gsub(pattern, "", x))
+corpus <- tm_map(corpus, remove_char, inToUtf8(8722))
+corpus <- tm_map(corpus, remove_char, inToUtf8(190))
+
+
+polish <- dictionary(lang="pl_PL")
+
+lemmatize <- function(text) {
+  
+  simple_text <- str_trim(as.character(text))
+  parsed_text <- strsplit(simple_text, split =" ")
+  new_text_vec <- hunspell_stem(parsed_text[[1]], dict=polish)
+  for ( i in 1:length(new_text_vec)) {
+    
+    if(lenght(new_text_vec[[1]]) == 0) new_text_vec[i] <- parsed_text[[1]][i]
+    if(lenght(new_text_vec[[1]]) > 1) new_text_vec[i] <- parsed_text[[i]][1]
+  }
+  new_text <- paste(new_text_vec, collapse = " ")  
+  return(new_text)
+  
+}
+
+corpus <- tm_map(corpus, content_transformer(lemmatize))
+
+# usuniêcie rozszerzenia z nazw plików
+cut_extensions <- function(document){
+  
+  meta(document, "id") <- gsub(pattern = "\\.txt$", replacement= "", meta(document, "id"))
+  return(document)
+}
+
+corpus <- tm_map(corpus, cut_extensions)
+
+# eksport zawartoœci korpusu do plików tekstowych
+
+preprocessedDir <- paste(outputDir, "Literatura - streszczenia - oryginaÅ‚",sep="\\")
+dir.create(preprocessedDir)
+writeCorpus(corpus, path = preprocessedDir)
+
+  
 writeLines(as.character(corpus[[1]]))
 writeLines(corpus[[1]]$content)
 
